2bf6b0c902c15878d4b21fdf2d74e311c65836f7
ozymeric <mail.ozanmeric@gmail.com>
2022-10-30 22:39:35 +0300

    added password to unlock the screen, added lock screen button to lock the screen and set timeout for screensaver

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 // _____________ VARIABLES:
 
-const formElement = document.querySelector(".form-element")
+const bodyElement = document.querySelector("body");
+const formElement = document.querySelector(".form-element");
 const textArea = document.querySelector("#text-area");
 const addButton = document.getElementById("add-button");
 const ulElement = document.querySelector("#ul");
-const saveButton = document.querySelector("#saveButtonDiv button")
+const saveButton = document.querySelector("#saveButtonDiv button");
 const deleteButton = document.querySelector("#delete");
 const clearAllButton = document.querySelector("#clear-all");
+const lockButton = document.querySelector("#lockButton");
+const passwordInput = document.querySelector("#password");
+const passwordButton = document.querySelector("#passwordButton");
+
+let myVar = setTimeout(lockScreen, 300000);
 
 // _____________ FUNCTIONS:
+
+function releaseNotebook(e) {
+  e.preventDefault();
+  if (passwordInput.value === "ozymeric") {
+    document.getElementById("outer").style.display = "block";
+    document.getElementById("log-in").style.display = "none";
+    passwordInput.value = "";
+  } else {
+    alert("Invalid Password!");
+    passwordInput.value = "";
+  }
+}
+
+function lockScreen() {
+  document.getElementById("outer").style.display = "none";
+  document.getElementById("log-in").style.display = "flex";
+}
+
+function lockScreenTimeout() {
+  clearTimeout(myVar);
+  myVar = setTimeout(lockScreen, 300000);
+}
 
 function fetchText(event) {
   event.preventDefault();
@@ -30,7 +58,7 @@ function save() {
   }
   const stringData = JSON.stringify(contentArray);
   console.log(stringData);
-  var file = new Blob([stringData],{type:"text"});
+  var file = new Blob([stringData], { type: "text" });
   const anchor = document.createElement("a");
   anchor.href = URL.createObjectURL(file);
   anchor.download = "notebook.txt";
@@ -39,8 +67,7 @@ function save() {
 
 function deleteLast() {
   allListElements = document.querySelectorAll("LI");
-  allListElements[allListElements.length -1].remove();
-
+  allListElements[allListElements.length - 1].remove();
 }
 
 function clearAll() {
@@ -51,11 +78,11 @@ function clearAll() {
 }
 
 function lineThrough() {
-  if (this.style.textDecoration === ("line-through")) {
+  if (this.style.textDecoration === "line-through") {
     this.remove();
   }
-  this.style.textDecoration = ("line-through");
-  this.style.color = ("grey");
+  this.style.textDecoration = "line-through";
+  this.style.color = "grey";
 }
 
 // _____________ EVENT LISTENERS:
@@ -64,3 +91,7 @@ addButton.addEventListener("click", fetchText);
 saveButton.addEventListener("click", save);
 deleteButton.addEventListener("click", deleteLast);
 clearAllButton.addEventListener("click", clearAll);
+passwordButton.addEventListener("click", releaseNotebook);
+bodyElement.addEventListener("mousemove", lockScreenTimeout);
+bodyElement.addEventListener("mousestop", lockScreenTimeout);
+lockButton.addEventListener("click", lockScreen);
